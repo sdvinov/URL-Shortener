6333a0e544e81693001840f4f8cc11681779fa6d
sdvinov <odvakr.wikia@gmail.com>
2016-12-07 03:58:34 -0500

    Added user tests

--- test/__links.js
+++ test/__links.js
@@ -7,7 +7,7 @@ const path = 'test/__links.js';
 describe('Links model test', () => {
   // Fake link
   const shortID = generate.randomValue(7);
-  let fakeLink = {
+  const fakeLink = {
     id: 9999,
     originLink: 'http://github.com/sdvinov',
     shortLinkID: shortID,
@@ -17,11 +17,9 @@ describe('Links model test', () => {
   it('Should create', (done) => {
     links.create(fakeLink, (err) => {
       util.debug(`Link was not created {${err}}`, path, 'e');
-    }, (originLink) => {
-      const fakeId = originLink.id;
-      const shortURL = originLink.shortID;
-      expect(originLink.originLink).to.be.equal(fakeLink.originLink);
-      expect(originLink.shortLinkID).to.be.equal(fakeLink.shortLinkID);
+    }, (link) => {
+      expect(link.originLink).to.be.equal(fakeLink.originLink);
+      expect(link.shortLinkID).to.be.equal(fakeLink.shortLinkID);
       done();
     });
   });
@@ -29,9 +27,9 @@ describe('Links model test', () => {
   // Read all
   it('Should read all', (done) => {
     links.findAll((err) => {
-      util.debug('All links were not found', path, 'e');
-    }, (links) => {
-      expect(links.length).to.be.above(0);
+      util.debug(`All links were not found {${err}}`, path, 'e');
+    }, (allLinks) => {
+      expect(allLinks.length).to.be.above(0);
       done();
     });
   });
@@ -39,7 +37,7 @@ describe('Links model test', () => {
   // Find one
   it('Should read by ID', (done) => {
     links.find(fakeLink, (err) => {
-      util.debug('Link by ID was not found', path, 'e');
+      util.debug(`Link by ID was not found {${err}}`, path, 'e');
     }, (link) => {
       expect(link.id).to.be.equal(fakeLink.id);
       done();
@@ -50,7 +48,7 @@ describe('Links model test', () => {
   it('Should update by ID', (done) => {
     fakeLink.originLink = 'http://github.com/sdvinov/URL-Shortener';
     links.update(fakeLink, (err) => {
-      util.debug('Link was updated', path, 'e');
+      util.debug(`Link was not updated {${err}}`, path, 'e');
     }, (link) => {
       expect(link.originLink).to.be.equal(fakeLink.originLink);
       done();
@@ -60,7 +58,7 @@ describe('Links model test', () => {
   // Delete
   it('Should delete', (done) => {
     links.destroy(fakeLink, (err) => {
-      util.debug('Link was not deleted', path, 'e');
+      util.debug(`User was not deleted {${err}}`, path, 'e');
     }, (success) => {
       expect(success).to.be.equal(1);
       done();
