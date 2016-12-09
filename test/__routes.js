af959af19b5d05080f1e2b1faaa56c01e4cc39fe
sdvinov <odvakr.wikia@gmail.com>
2016-12-08 19:38:59 -0500

    Getting there

--- test/__routes.js
+++ test/__routes.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const expect = require('chai').expect;
-const app = require('../src/app');
 const generate = require('../src/modules/generator');
 
 const shortID = generate.randomValue(7);
@@ -11,36 +10,48 @@ const fakeLink = {
 };
 
 describe('Routes tests', () => {
-  it('Should load all links', (done) => {
-    request(app)
-      .get('/api/v1/urls')
-      .set('Accept', 'application/json')
+  let server;
+  beforeEach(() => {
+    server = require('./../src/app');
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+  
+  it('GET /api/v1/urls', (done) => {
+    request(server)
+      .get('api/v1/urls')
+      .set('Accept', 'serverlication/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(done);
   });
 
-  it('Should load one link', (done) => {
-    request(app)
-      .get('/api/v1/urls/:id')
-      .set('Accept', 'application/json')
+  it('POST /api/v1/url', (done) => {
+    request(server)
+      .post('api/v1/url')
+      .set('Accept', 'serverlication/json')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .end(done);
-  });
-
-  it('Should POST new link', (done) => {
-    request(app)
-      .post('/api/v1/url')
       .send(fakeLink)
       .expect(200)
       .end(done);
   });
 
-  it('Should DELETE one link', (done) => {
-    request(app)
-      .delete('/api/v1/urls/9998')
-      .expect('Content-Length', '1')
+  it('GET /api/v1/url/:id', (done) => {
+    request(server)
+      .get('api/v1/url/:id')
+      .set('Accept', 'serverlication/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(done);
+  });
+
+  it('DELETE /api/v1/url/:id', (done) => {
+    request(server)
+      .delete('api/v1/url/:id')
+      .set('Accept', 'serverlication/json')
+      .expect('Content-Type', /json/)
       .expect(200)
       .end(done);
   });
