9c8e99b3594df95b52a241402f211eb1b1ffdf54
sdvinov <odvakr.wikia@gmail.com>
2016-12-08 20:14:58 -0500

    Array of routes

--- test/__routes.js
+++ test/__routes.js
@@ -2,12 +2,63 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const generate = require('../src/modules/generator');
 
-const shortID = generate.randomValue(7);
-const fakeLink = {
-  id: 9998,
-  originLink: 'http://github.com/sdvinov',
-  shortLinkID: shortID,
-};
+const allRoutes = [
+  {
+    testName: 'Get all links (GET /api/v1/urls)',
+    url: '/api/v1/urls',
+    method: 'get',
+  },
+  {
+    testName: 'Get one link (GET /api/v1/url/:id)',
+    url: '/api/v1/url/:id',
+    method: 'get',
+  },
+  {
+    testName: 'Get redirected (GET /go/:shortLinkID',
+    url: '/go/:shortLinkID',
+    method: 'get',
+  },
+  {
+    testName: 'Get all users (GET /api/v1/users)',
+    url: '/api/v1/users',
+    method: 'get',
+  },
+  {
+    testName: 'Get one user (GET /api/v1/user/:id)',
+    url: '/api/v1/user/:id',
+    method: 'get',
+  },
+  {
+    testName: 'Post new link (POST /api/v1/url)',
+    url: '/api/v1/url',
+    method: 'post',
+  },
+  {
+    testName: 'Update a link (POST /api/v1/urls/:id)',
+    url: '/api/v1/urls/:id',
+    method: 'post',
+  },
+  {
+    testName: 'Post new user (POST /api/v1/user)',
+    url: '/api/v1/user',
+    method: 'post',
+  },
+  {
+    testName: 'Update a user (POST /api/v1/users/:id)',
+    url: '/api/v1/users/:id',
+    method: 'post',
+  },
+  {
+    testName: 'Delete link (DELETE /api/v1/urls/:id)',
+    url: '/api/v1/urls/:id',
+    method: 'delete',
+  },
+  {
+    testName: 'Delete a user (POST /api/v1/users/:id)',
+    url: '/api/v1/users/:id',
+    method: 'post',
+  },
+];
 
 describe('Routes tests', () => {
   let server;
@@ -18,7 +69,7 @@ describe('Routes tests', () => {
   afterEach(() => {
     server.close();
   });
-  
+
   it('GET /api/v1/urls', (done) => {
     request(server)
       .get('api/v1/urls')
