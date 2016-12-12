611254aecd663b99dc6dd9309aa78cce39d47f8d
sdvinov <odvakr.wikia@gmail.com>
2016-12-11 19:49:27 -0500

    Sending files to server

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ module.exports = (express) => {
   const router = express.Router();
 
   // Configuring to use /api prefix for api routes
+  
   router.use('/api/v1', require('./api/api')(express));
   router.use('/api/v1', require('./user/user')(express));
   router.use('/go', require('./go/redirect')(express));
