3d02a58fcafa9d17f74bb463dabbaa1d6276231c
sdvinov <odvakr.wikia@gmail.com>
2016-12-11 19:50:55 -0500

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ module.exports = (express) => {
   const router = express.Router();
 
   // Configuring to use /api prefix for api routes
-  
   router.use('/api/v1', require('./api/api')(express));
   router.use('/api/v1', require('./user/user')(express));
   router.use('/go', require('./go/redirect')(express));
