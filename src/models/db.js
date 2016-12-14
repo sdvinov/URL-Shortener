858b4d66c5879c7e0e434d08a5362014e52e1995
sdvinov <odvakr.wikia@gmail.com>
2016-12-13 19:25:52 -0500

    Fixed dependencies

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,4 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
-
 
 // Configuring database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
